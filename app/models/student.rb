aec9c4694286f770a90e0e73662312ddc80efd62
Your Name <you@example.com>
2017-11-06 18:44:24 +0100

    Smutek

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   has_and_belongs_to_many :courses, :join_table => :students_courses
   has_many :topics
   has_many :posts
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: true
+  validates :index, presence: true, length: { minimum: 6, maximum: 6 }, uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root to: 'static#index'
   resources :posts
   resources :topics
   resources :courses
   resources :students
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
